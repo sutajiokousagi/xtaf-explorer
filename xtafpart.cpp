e5daaf5e921df15346dce39a4b94ae8f6828f54f
Sean Cross <smcross@gmail.com>
2012-07-10 09:57:33 +0200

    xtafpart: Add xtaf partition management class
    Xtafpart allows the user to select a partition off of an Xbox 360
    disk image.  It uses a hardcoded partition table, and abstracts
    each partition away.

---
+++ xtafpart.cpp
@@ -0,0 +1,145 @@
+#include <QDebug>
+#include "xtafpart.h"
+
+
+struct partition_def {
+        const char              *name;
+        quint64                 offset;
+        quint64                 length;
+        enum XtafPart::Format   format;
+};
+
+
+struct part {
+        quint32                 id;
+        struct partition_def    *def;
+        quint8                  *data;
+        struct disk             *disk;
+};
+
+
+/* Hardcoded into the Xbox 360 Phat's ROM */
+static struct partition_def partitions[] = {
+        {
+                "Security Sector",
+                0x2000ULL,
+                0x40ULL,
+                XtafPart::None,
+        },
+        {
+                "System Cache",
+                0x80000ULL,
+                0x80000000ULL,
+                XtafPart::SFCX,
+        },
+        {
+                "Game Cache",
+                0x80080000ULL,
+                0xA0E30000ULL,
+                XtafPart::SFCX,
+        },
+        {
+                "SysExt",
+                0x10C080000ULL,
+                0xCE30000ULL,
+                XtafPart::XTAF,
+        },
+        {
+                "SysExt2",
+                0x118EB0000ULL,
+                0xCE30000ULL,
+                XtafPart::XTAF,
+        },
+        {
+                "Xbox 1",
+                0x120eb0000ULL,
+                0x10000000ULL,
+                XtafPart::XTAF,
+        },
+
+        // "Data" partition takes up the rest of the disk
+        {
+                "Data",
+                0x130eb0000ULL,
+                0,
+                XtafPart::XTAF,
+        },
+};
+
+XtafPart::XtafPart(QObject *parent) :
+    QObject(parent),
+    currentPartition(-1)
+{
+}
+
+
+unsigned int XtafPart::count(void)
+{
+    return sizeof(partitions)/sizeof(*partitions);
+}
+
+const char *XtafPart::name(quint8 part_id)
+{
+    return part_id < count() ? partitions[part_id].name : NULL;
+}
+
+enum XtafPart::Format XtafPart::format(quint8 part_id)
+{
+    if (part_id >= count())
+        return XtafPart::None;
+    return partitions[part_id].format;
+}
+
+
+
+enum XtafPart::Format XtafPart::format(void)
+{
+    if (currentPartition >= count() || currentPartition < 0)
+        return XtafPart::Invalid;
+    return partitions[currentPartition].format;
+}
+
+
+int XtafPart::setPartition(quint8 part_id)
+{
+    if (part_id >= count())
+        return -1;
+    currentPartition = part_id;
+        return 0;
+}
+
+int XtafPart::setDisk(XtafDisk *newDisk)
+{
+    if (!newDisk)
+        return -1;
+    disk = newDisk;
+    return 0;
+}
+
+
+qint64 XtafPart::length(void) {
+    qint64 length;
+
+    if (!disk || currentPartition < 0)
+        return -1;
+
+    length = partitions[currentPartition].length;
+
+    if (!length)
+        length = disk->size() - partitions[currentPartition].offset;
+    return length;
+}
+
+int XtafPart::read(quint64 offset, void *bytes, quint64 size)
+{
+    if (!disk || currentPartition < 0)
+        return -1;
+
+    if (offset > (quint64)length())
+        return -1;
+    if (offset+size > (quint64)length())
+        size = length()-offset;
+
+    offset += partitions[currentPartition].offset;
+    return disk->read(offset, (char *)bytes, size);
+}
---
+++ xtafpart.h
@@ -0,0 +1,39 @@
+#ifndef XTAFPART_H
+#define XTAFPART_H
+
+#include <QObject>
+#include "xtafdisk.h"
+
+
+class XtafPart : public QObject
+{
+	Q_OBJECT
+public:    
+                enum Format {
+                        Invalid,
+                        None,
+                        SFCX,
+                        XTAF,
+                };
+                explicit XtafPart(QObject *parent = 0);
+                unsigned int count(void);	// Returns the number of partitions
+                const char *name(quint8 partition);
+                enum Format format(quint8 partition);
+                enum Format format(void);
+                int setPartition(quint8 partition);
+                int setDisk(XtafDisk *disk);
+
+                qint64 length(void);
+                int read(quint64 offset, void *bytes, quint64 size);
+
+private:
+                int currentPartition;
+                XtafDisk *disk;
+
+signals:
+	
+public slots:
+	
+};
+
+#endif // XTAFPART_H
