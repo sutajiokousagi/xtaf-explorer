5b6d5e0edcc592d18d9e6be358fef4df0770c093
Sean Cross <smcross@gmail.com>
2012-07-10 09:54:54 +0200

    xtaffilesystemmodel: Add class
    This model hooks up to a tree view, and provides a model driver for
    viewing an Xtaf filesystem.

---
+++ xtaffilesystemmodel.cpp
@@ -0,0 +1,189 @@
+#include <QDebug>
+#include "xtaffilesystemmodel.h"
+
+
+
+XtafFileSystemModel::XtafFileSystemModel(QObject *parent) :
+    QAbstractItemModel(parent)
+{
+}
+
+QModelIndex XtafFileSystemModel::index(const QModelIndex & parent) const
+{
+    return rootIndex;
+}
+
+QModelIndex XtafFileSystemModel::index(int row, int column, const QModelIndex & parent) const
+{
+    if (!hasIndex(row, column, parent))
+        return QModelIndex();
+
+    if (!parent.isValid())
+        return createIndex(row, column, fsys->rootDirectory().entries()[row]);
+
+    XtafFile *file = static_cast<XtafFile *>(parent.internalPointer());
+    if (!file)
+        qWarning() << "Index is NULL";
+    XtafDirectory *dir = (XtafDirectory *)file;
+    return createIndex(row, column, dir->entries()[row]);
+}
+
+QModelIndex XtafFileSystemModel::parent(const QModelIndex & index) const
+{
+    if (!index.isValid())
+        return QModelIndex();
+
+    XtafFile *file = static_cast<XtafFile *>(index.internalPointer());
+    if (!file)
+        qWarning() << "Parent is NULL";
+    XtafDirectory *parentDir = (XtafDirectory *)file->parent();
+
+    if (parentDir == &(fsys->rootDirectory()))
+        return QModelIndex();
+
+    int i;
+    for (i=0; i<parentDir->entries().size(); i++) {
+        if (parentDir->entries().at(i) == file)
+            return createIndex(i, 0, parentDir);
+    }
+
+    return QModelIndex();
+}
+
+int XtafFileSystemModel::rowCount(const QModelIndex & parent) const
+{
+    XtafFile *file;
+    XtafDirectory *dir;
+    if (!fsys) {
+        qDebug() << "No filesystem object";
+        return 0;
+    }
+
+    if (parent.column() > 0)
+        return 0;
+
+    file = static_cast<XtafFile *>(parent.internalPointer());
+    if (!file) {
+        return fsys->rootDirectory().entries().size();
+    }
+
+    if (!file->isDirectory()) {
+        return 0;
+    }
+
+    dir = (XtafDirectory *)file;
+    return dir->entries().size();
+}
+
+int XtafFileSystemModel::columnCount(const QModelIndex & parent) const
+{
+    return 5;
+}
+
+QVariant XtafFileSystemModel::data(const QModelIndex & index, int role) const
+{
+    XtafFile *f = static_cast<XtafFile *>(index.internalPointer());
+    if (!f)
+        qWarning() << "Data model is NULL";
+
+    if (role == Qt::SizeHintRole) {
+        if (index.column() == 0)
+            return QVariant(QSize(400,24));
+        else
+            return QVariant(QSize(50,24));
+    }
+
+    else if (role == Qt::DisplayRole) {
+        if (index.column() == 0)
+            return QVariant(QString(f->name()));
+        if (index.column() == 1)
+            return QVariant(f->size());
+        if (index.column() == 2)
+            return QVariant(f->created().toString(Qt::SystemLocaleShortDate));
+        if (index.column() == 3)
+            return QVariant(f->updated().toString(Qt::SystemLocaleShortDate));
+        if (index.column() == 4)
+            return QVariant(f->accessed().toString(Qt::SystemLocaleShortDate));
+        return QVariant(QString("???"));
+    }
+
+    else if (role == Qt::DecorationRole) {
+        return QVariant();
+    }
+
+    else if (role == Qt::ToolTipRole) {
+        return QVariant();
+    }
+
+    else if (role == Qt::FontRole)
+        return QVariant();
+
+    else if (role == Qt::TextAlignmentRole)
+        return QVariant();
+
+    else if (role == Qt::BackgroundColorRole) {
+        if (f->isDeleted())
+            return QColor::fromRgb(255,0,0);
+        return QVariant();
+    }
+
+    else if (role == Qt::TextColorRole)
+        return QVariant();
+
+    else if (role == Qt::CheckStateRole)
+        return QVariant();
+
+    qDebug() << "Unknown item role" << role;
+    return QVariant();
+}
+
+int XtafFileSystemModel::setXtafFilesystem(XtafFsys *xtafFsys)
+{
+    fsys = xtafFsys;
+    emit dataChanged(rootIndex, rootIndex);
+    return 0;
+}
+
+
+bool XtafFileSystemModel::hasChildren(const QModelIndex & parent) const
+{
+    // Special-case for root object
+    if (!parent.isValid())
+        return true;
+    XtafFile *f = static_cast<XtafFile *>(parent.internalPointer());
+    return f->isDirectory();
+}
+
+Qt::ItemFlags XtafFileSystemModel::flags(const QModelIndex & index) const
+{
+    return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
+}
+
+QVariant XtafFileSystemModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role == Qt::SizeHintRole) {
+        if (section == 0)
+            return QVariant(QSize(400,24));
+        else
+            return QVariant(QSize(50,24));
+    }
+
+    else if (role == Qt::DisplayRole) {
+        if (section == 0)
+            return QVariant(QString("Name"));
+        else if (section == 1)
+            return QVariant(QString("Size"));
+        else if (section == 2)
+            return QVariant(QString("Created"));
+        else if (section == 3)
+            return QVariant(QString("Modified"));
+        else if (section == 4)
+            return QVariant(QString("Accessed"));
+        else
+            return QVariant(QString("????"));
+    }
+
+//    qDebug() << "Unknown item role" << role;
+    return QVariant();
+}
+
---
+++ xtaffilesystemmodel.h
@@ -0,0 +1,38 @@
+#ifndef XTAFFILESYSTEMMODEL_H
+#define XTAFFILESYSTEMMODEL_H
+
+#include <QFileSystemModel>
+
+#include "xtaffsys.h"
+
+class XtafFileSystemModel : public QAbstractItemModel
+{
+    Q_OBJECT
+public:
+    explicit XtafFileSystemModel(QObject *parent = 0);
+    void setReadOnly(bool enable);
+    bool isReadOnly() const;
+
+    QModelIndex index(int row, int column, const QModelIndex & parent = QModelIndex()) const;
+    QModelIndex index(const QModelIndex & parent = QModelIndex()) const;
+    QModelIndex parent(const QModelIndex & index) const;
+    int rowCount(const QModelIndex & parent = QModelIndex()) const;
+    int columnCount(const QModelIndex & parent = QModelIndex()) const;
+    QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
+    bool hasChildren(const QModelIndex & parent = QModelIndex()) const;
+    Qt::ItemFlags flags(const QModelIndex & index) const;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+
+    int setXtafFilesystem(XtafFsys *fsys);
+
+private:
+    XtafFsys *fsys;
+    QModelIndex rootIndex;
+
+signals:
+    
+public slots:
+    
+};
+
+#endif // XTAFFILESYSTEMMODEL_H
